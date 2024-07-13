d9cdad5efb4c8fa6fa43fc8ca323f9ec2c66158a
jonathan <jonathan20cba@gmail.com>
2024-07-13 01:43:16 -0300

    Se agrega conexión remota a bd

--- server/config/mysql.js
+++ server/config/mysql.js
@@ -2,15 +2,16 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const database = "prendas";
-const username = "root";
-const password = "";
-const host = "localhost";
+const database = process.env.DB_NAME || "prendas";
+const username = process.env.DB_USER || "root";
+const password = process.env.DB_PASS || "";
+const host = process.env.DB_HOST || "localhost";
+const port = process.env.DB_PORT || 3307;
 
 const sequelize = new Sequelize(database, username, password, {
   host: host,
   dialect: "mysql",
-  port: 3307,
+  port: port,
 });
 
 const dbConnectionMySql = async () => {
